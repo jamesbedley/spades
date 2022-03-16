17593bf717036c0eb57afa8cbfebedeff4931760
abedley <3718861+abedley@users.noreply.github.com>
2022-03-16 18:01:29 -0400

    unify file system, fix bugs, refresh on save, open on start

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+const connectLivereload = require('connect-livereload');
 const express = require('express');
+const livereload = require('livereload');
 const serveStatic = require('serve-static');
 const path = require('path');
 
+////
 
-const app=express();
+const liveReloadServer = livereload.createServer();
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
+////
+
+const app = express();
+
+app.use(connectLivereload());
+app.use('/spades', serveStatic(path.join(__dirname, 'public')));
 
 app.listen(3000); //port
